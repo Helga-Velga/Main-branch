4018db3c10cde89afddeb00a2a6f34dbc3ee01b4
Helga-Velga <helga-velgaaaaa@gmail.com>
2025-09-04 13:58:23 +0300

    Lesson 2.5 Expections

---
+++ src/Expections/ArrayProcessor.java
@@ -0,0 +1,66 @@
+package Exceptions;
+
+// Исключение при неправильном размере массива
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+// Исключение при невозможности преобразовать элемент массива в число
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
+
+public class ArrayProcessor {
+
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Неверное количество строк. Ожидается 4.");
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Неверное количество столбцов в строке " + i + ". Ожидается 4.");
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        // Пример массива
+        String[][] correctArray = {
+                {"1", "2", "3", "4"},
+                {"5", "v", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "X", "16"}
+        };
+
+        try {
+            int result = processArray(correctArray);
+            System.out.println("Сумма элементов массива: " + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных массива: " + e.getMessage());
+        }
+        try {
+            int[] arr = {1, 2, 3};
+            System.out.println("Доступ к элементу по неправильному индексу: " + arr[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Поймано исключение ArrayIndexOutOfBoundsException: " + e.getMessage());
+        }
+    }
+}
